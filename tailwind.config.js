7f67d17548b5499fd16c066d32a083759793fd6e
Tushar Ojha <tusharojha96@gmail.com>
2022-11-10 21:58:50 +0530

    almost done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/assets/Paytm-money-background')"
+      }
+    },
+  },
+  plugins: [],
+}
